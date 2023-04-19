91bd64ec9ac338ee4c8c5ee0eceab37e84de4204
Arif Dikkanat <dikkanata@mef.edu.tr>
2023-04-20 01:38:15 +0300

    translation method added

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 First, you have to install the following dependencies: (you can install them using either pip, pip3 or conda)
 
-````
+```
 python
 uvicorn
 fastapi
@@ -12,59 +12,61 @@ torch
 PIL
 random
 diffusers
-````
+transformers
+langdetect
+```
 
 ## Information about GPU Training
 
 If you are going to run this program on a MacOS device with Apple Silicon, you are going select the device as "mps" (Metal Performance Shaders) for GPU training acceleration.
 This code repository is created for this type of devices, so the following codes in api.py file are related to that:
 
-````
+```
 device = "mps" // if you are going to use a windows computer, change this to CUDA and install it to your computer.
-````
+```
 
 ## Running the Program
 
-Before you run the program, you have to create a new file called ````auth_token.py````
+Before you run the program, you have to create a new file called `auth_token.py`
 
 This file will store your hugging face access key and it will be ignored via .gitignore file
 
 This file should have the following code:
-````
+
+```
 auth_token = "[YOUR_HUGGING_FACE_ACCESS_KEY]"
-````
+```
 
 In order to start this program run the following command:
-````
-uvicorn api:app --reload
-````
 
-After you run the program, it will start on port *127.0.0.1:8000*,
+```
+uvicorn api:app --reload
+```
+
+After you run the program, it will start on port _127.0.0.1:8000_,
 
 Then, you can go to the user interface on *http://127.0.0.1:8000/docs#*
 
-
-
 ## Structure of the API
 
-Right now there is one endpoint "/" which is used to generate image to image transitions. 
+Right now there is one endpoint "/" which is used to generate image to image transitions.
 
 The endpoint takes three inputs:
+
 - prompt: The command that users are giving to the model for desired changes,
 - url: The url of the image that they desire to change,
 - title: Title they want to give to the generated image
 
-The new generated image is stored to images directory, and can be accessed from: 
-````http://127.0.0.1:8000/images/[imagetitle.png]````
+The new generated image is stored to images directory, and can be accessed from:
+`http://127.0.0.1:8000/images/[imagetitle.png]`
 
 The response of this endpoint is a JSON object with the following data fields:
-````
-{ "image": 
+
+```
+{ "image":
   {
     "src": new generated image url
     "alt": title the user have provied
   }
 }
-````
-
-
+```
--- api.py
+++ api.py
@@ -4,29 +4,34 @@ from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from fastapi.staticfiles import StaticFiles
 import torch
-from diffusers import  StableDiffusionInstructPix2PixPipeline, EulerAncestralDiscreteScheduler
+from diffusers import StableDiffusionInstructPix2PixPipeline, EulerAncestralDiscreteScheduler
 import PIL
 import requests
 import random
 from auth_token import auth_token
+from transformers import MarianMTModel, MarianTokenizer
+from langdetect import detect
 
 app = FastAPI()
 
 app.add_middleware(
-    CORSMiddleware, 
-    allow_credentials=True, 
-    allow_origins=["*"], 
-    allow_methods=["*"], 
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=["*"],
+    allow_methods=["*"],
     allow_headers=["*"]
 )
 
 device = "mps"
 model_id = "timbrooks/instruct-pix2pix"
-pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(model_id, torch_dtype=torch.float16, safety_checker=None, use_auth_token=auth_token)
+pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained(
+    model_id, torch_dtype=torch.float16, safety_checker=None, use_auth_token=auth_token)
 
 pipe.to(device)
-pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
+pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(
+    pipe.scheduler.config)
 pipe.enable_attention_slicing()
+
 
 def download_image(url):
     image = PIL.Image.open(requests.get(url, stream=True).raw)
@@ -34,20 +39,44 @@ def download_image(url):
     image = image.convert("RGB")
     return image
 
-@app.get("/")
-def generate(prompt: str, url: str, title: str): 
+
+@app.get("/imageProcess")
+def generate(prompt: str, url: str, title: str):
     img = download_image(url)
-    image = pipe(prompt, image=img, num_inference_steps=10, image_guidance_scale=1).images[0]
-    num = random.randint(0,10000000000)
+    image = pipe(prompt, image=img, num_inference_steps=10,
+                 image_guidance_scale=1).images[0]
+    num = random.randint(0, 10000000000)
     imgPath = f"images/{str(num) + title}.png"
     image.save(imgPath)
- 
+
     imageSource = f"http://127.0.0.1:8000/{imgPath}"
-    resData = { "image": {
+    resData = {"image": {
         "src": imageSource,
         "alt": title
-        }
+    }
     }
     return JSONResponse(content=jsonable_encoder(resData))
+
+
+@app.get("/translation")
+def generate2(input_text: str):
+    input_text = input_text
+    input_lang = detect(input_text)
+    output_text = input_text
+    if input_lang != 'en':
+        model_name = f'Helsinki-NLP/opus-mt-{input_lang}-en'
+        tokenizer = MarianTokenizer.from_pretrained(model_name)
+        model = MarianMTModel.from_pretrained(model_name)
+        inputs = tokenizer(input_text, return_tensors="pt", truncation=True)
+        outputs = model.generate(**inputs)
+        output_text = tokenizer.batch_decode(
+            outputs, skip_special_tokens=True)[0]
+
+        resData = {"translation": {
+            "translatedText": output_text
+        }
+        }
+    return JSONResponse(content=jsonable_encoder(resData))
+
 
 app.mount("/images", StaticFiles(directory="images"), name="images")
